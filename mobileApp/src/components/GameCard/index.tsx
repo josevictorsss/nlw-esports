70c46afcbe7d2f3d1a134bf11b559b7d8836e51d
josevictorsss <josevictortf@gmail.com>
2022-09-12 22:21:07 -0300

    feat: implement GameCard on Home

---
+++ mobileApp/src/components/GameCard/index.tsx
@@ -0,0 +1,34 @@
+import { LinearGradient } from 'expo-linear-gradient';
+import {
+  ImageBackground,
+  ImageSourcePropType,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+import { THEME } from '../../theme';
+
+import { styles } from './styles';
+
+export interface GameCardProps {
+  id: string;
+  name: string;
+  ads: string;
+  cover: ImageSourcePropType;
+}
+
+interface Props {
+  data: GameCardProps;
+}
+
+export function GameCard({ data, ...props }: Props) {
+  return (
+    <TouchableOpacity style={styles.container}>
+      <ImageBackground style={styles.cover} source={data.cover}>
+        <LinearGradient colors={THEME.COLORS.FOOTER} style={styles.footer}>
+          <Text style={styles.name}>{data.name}</Text>
+          <Text style={styles.ads}>{data.ads} anúncios</Text>
+        </LinearGradient>
+      </ImageBackground>
+    </TouchableOpacity>
+  );
+}
--- mobileApp/src/screens/Home/index.tsx
+++ mobileApp/src/screens/Home/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Image, View } from 'react-native';
+import { FlatList, Image, View } from 'react-native';
 import { styles } from './styles';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
 import { Heading } from '../../components/Heading';
+import { GameCard } from '../../components/GameCard';
+import { GAMES } from '../../utils/games';
 
 export function Home() {
   return (
@@ -12,6 +14,14 @@ export function Home() {
       <Heading
         title="Encontre seu duo!"
         subtitle="Selecione o game que deseja jogar..."
+      />
+      <FlatList
+        data={GAMES}
+        contentContainerStyle={styles.contentList}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <GameCard data={item} />}
+        horizontal
+        showsHorizontalScrollIndicator={false}
       />
     </View>
   );
