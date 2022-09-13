47ea0fb2d0ce1cd37c0dbff1492629379359665e
josevictorsss <josevictortf@gmail.com>
2022-09-13 20:00:01 -0300

    chore: add prisma

--- back/src/server.ts
+++ back/src/server.ts
@@ -2,8 +2,20 @@ import express, { response } from 'express';
 
 const app = express();
 
-app.get('/ads', (req, res) => {
-  return response.send('Hello World');
+app.get('/games', (req, res) => {
+  return response.json([]);
+});
+
+app.post('/ads', (req, res) => {
+  return response.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (req, res) => {
+  return response.json([]);
+});
+
+app.get('/ads/:id/discord', (req, res) => {
+  return response.json([]);
 });
 
 app.listen(3333);
