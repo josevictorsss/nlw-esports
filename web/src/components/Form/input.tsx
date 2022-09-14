fe67d492c179cb9d6d65c78fe5193dc9de5cc1cd
josevictorsss <josevictortf@gmail.com>
2022-09-14 20:49:24 -0300

    feat: modal created

---
+++ web/src/components/Form/input.tsx
@@ -0,0 +1,10 @@
+import { InputHTMLAttributes } from 'react';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+export const Input = (props: InputProps) => (
+  <input
+    {...props}
+    className="bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500"
+  />
+);
