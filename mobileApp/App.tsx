145891aa346568c75444756b3a35393c38550133
josevictorsss <josevictortf@gmail.com>
2022-09-18 13:03:16 -0300

    feat: ensure Notifications arrive to app users

--- mobileApp/App.tsx
+++ mobileApp/App.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useRef } from 'react';
+import { Subscription } from 'expo-modules-core';
+import * as Notifications from 'expo-notifications';
 import { StatusBar } from 'react-native';
 import { Background } from './src/components/Background';
 import {
@@ -10,6 +13,9 @@ import {
 import { Routes } from './src/routes';
 import { Loader } from './src/components/Loader';
 
+import './src/services/notificationConfigs';
+import { getPushNotificationToken } from './src/services/getPushNotificationToken';
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
@@ -17,6 +23,39 @@ export default function App() {
     Inter_700Bold,
     Inter_900Black,
   });
+
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken();
+  }, []);
+
+  useEffect(() => {
+    getNotificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        console.log(notification);
+      });
+
+    responseNotificationListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      if (
+        getNotificationListener.current &&
+        responseNotificationListener.current
+      ) {
+        Notifications.removeNotificationSubscription(
+          getNotificationListener.current
+        );
+        Notifications.removeNotificationSubscription(
+          responseNotificationListener.current
+        );
+      }
+    };
+  }, []);
 
   return (
     <Background>
